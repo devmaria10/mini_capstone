bab6d7cc7373f0ebba9374e328e8bf8f0933933c
Maria <mariajescobar10@gmail.com>
2018-01-18 21:24:14 -0600

    more stuff

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,41 @@
 class ProductsController < ApplicationController
-  def all_products_method
+  def index
     products = Product.all
     render json: products.as_json
   end 
+
+  def create
+    product = Product.new(
+                          name: params[:name],
+                          price: params[:chef],
+                          image_url: params[:image_url],
+                          description: params[:description]
+                          )
+    product.save
+    render json: product.as_json 
+  end 
+
+  def show
+    product = Product.find(params[:id])
+    render json: product.as_json 
+  end 
+
+  def update
+    product = Product.find(params[:id])
+    
+    product.name = params[:name]
+    product.price = params[:price]
+    product.image_url = params[:image_url]
+    product.description = params[:description]
+    product.save 
+
+    render json: product.as_json 
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: { message: "Successfully deleted Product ##{product.id}." }
+  end  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get '/all_products_url' => 'products#all_products_method'
+  get '/products' => 'products#index'
+  post '/products' => 'products#create'
+  get '/products/:id' => 'products#show'
+  patch '/products/:id' => 'products#update'
+  delete '/products/:id' => 'products#destroy'
 end
