a277f8b0448ecdc789a814cfe7dffed8361dca40
Maria <mariajescobar10@gmail.com>
2018-01-30 18:30:55 -0600

    added User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
                   name: params[:name],
                   email: params[:email],
                   password: params[:password],
-                  password_confirmation: parmas[:password_confirmation] 
+                  password_confirmation: params[:password_confirmation] 
     )
+    if user.save
+      render json: {message: 'User created successfully'}, status: :created
+    else 
+      render json: {errors: user.errors.full_messages}, status: :bad_request
+    end 
 end
